06bd8028685ce7a85d2e5d7052df3b7dd40065ac
Royce Dy <rady182@me.com>
2018-07-27 02:10:51 +0800

    test update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express yo' });
+  res.render('index', { title: 'Express with cache daw?' });
 });
 
 module.exports = router;
