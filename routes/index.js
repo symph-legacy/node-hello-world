157ad58df52861c7d2571c151b4d939e4b102112
Royce Dy <rady182@me.com>
2018-07-27 16:51:42 +0800

    0 deregister delay

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express updated deregister value' });
+  res.render('index', { title: 'Express updated deregister 0 value' });
 });
 
 module.exports = router;
