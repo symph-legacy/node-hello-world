4e5c59d6a1b3231bab4d1515125ea849a1015182
Royce Dy <rady182@me.com>
2018-07-27 17:36:09 +0800

    new cache update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express updated sempahore ci cache update' });
+  res.render('index', {
+    title: 'Express updated sempahore ci new cache update'
+  });
 });
 
 module.exports = router;
